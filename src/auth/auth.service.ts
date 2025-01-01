036c48360854fb376830c12b2ec1a89399967057
Ore Adeleye <oreadeleye67@gmail.com>
2025-01-01 03:45:44 +0100

    Register and login

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,42 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { User } from '@prisma/client';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+  ) {}
+
+  async register(username: string, email: string, password: string) {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await this.prisma.user.create({
+      data: { username, email, password: hashedPassword },
+    });
+
+    const token = this.generateToken(user.id, user.email);
+    return { user: this.excludePassword(user), token };
+  }
+
+  async login(email: string, password: string) {
+    const user = await this.prisma.user.findUnique({ where: { email } });
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      throw new UnauthorizedException('Invalid email or password');
+    }
+
+    const token = this.generateToken(user.id, user.email);
+    return { user: this.excludePassword(user), token };
+  }
+
+  private generateToken(userId: number, email: string) {
+    return this.jwtService.sign({ sub: userId, email });
+  }
+
+  private excludePassword({ ...user }: User) {
+    delete user.password;
+    return user;
+  }
+}
